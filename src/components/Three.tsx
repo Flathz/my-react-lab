64306a897a154d142b712e5c5af23858ce825245
Charif <flath1995@gmail.com>
2024-07-25 14:55:27 +0200

    feat, working car but ts errors on the model that i need to fix

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -1,13 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Environment, OrbitControls } from "@react-three/drei";
+import { Car } from "../models/Car";
 
 function Three() {
   return (
-    <Canvas>
+    <Canvas eventPrefix="layer">
       <Suspense fallback={null}>
-        <Environment preset="city" background />
-        <OrbitControls />
+        <Environment  preset="city" background />
+        <Car/>
+        <OrbitControls attach={'Car'}/>
       </Suspense>
     </Canvas>
   );
