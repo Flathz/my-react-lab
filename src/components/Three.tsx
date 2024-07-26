4b55ea74221f789d59533dfc9d79f51765ae451a
Charif <flath1995@gmail.com>
2024-07-26 16:32:37 +0200

    wip, isometric camera done, need to update sky

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -1,14 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Car } from "../models/Car";
-import { OrbitControls } from "@react-three/drei";
 
 function Three() {
+
   return (
-    <Canvas eventPrefix="layer">
+
+    <Canvas camera={{ near: 0.1, far: 1000, position: [-20, 4, -10]} }>
       <ambientLight intensity={1} />
       <directionalLight color="white" position={[0, 0, 5]} />
-      <Car position={[0, -5,-7]} />
-      <OrbitControls enableZoom={false} />
+      <Car position={[0, -5, -7]} />
     </Canvas>
   );
 }
